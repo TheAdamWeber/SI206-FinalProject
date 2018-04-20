9a00694bfe19d88b313cb0e3c241b1381af09e79
TheAdamWeber <ajweber@umich.edu>
2018-04-19 23:30:45 -0400

    Added boilerplate secret_data.py

---
+++ secret_data.py
@@ -0,0 +1,8 @@
+## API Keys                                                                                 
+CLIENT_ID = ''                                                                       
+CLIENT_SECRET = ''                                                     
+USER_AGENT = 'application to scrape top posts on reddit'                                    
+TWITTER_CONSUMER_KEY = ''                                               
+TWITTER_CONSUMER_SECRET = ''                     
+TWITTER_ACCESS_KEY = ''                        
+TWITTER_ACCESS_SECRET = ''     
